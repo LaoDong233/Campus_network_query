57c1685ed965f28fe3516a5299e0080ba3790f92
Lao_Dong <xianyudelaodong@outlook.com>
2021-11-17 11:17:35 +0800

    添加其他学校版本，理论通杀锐捷认证系统

--- link.py
+++ link.py
@@ -2,11 +2,14 @@ import requests.utils
 
 
 class Link:
-    def __init__(self, username, password):
+    def __init__(self, link, username, password):
         self.data = {
             'name': username,
             'password': password
         }
+        self.link = link
+        self.login = "module/scgroup/web/login_judge.jsf"
+        self.query = "module/webcontent/web/onlinedevice_list.jsf"
         self.header = {
             "User-Agent": "Mozilla/5.0 (Linux; Android 7.1.2; PCRT00 Build/N2G48H; wv) AppleWebKit/537.36 "
                           "(KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.70 Mobile Safari/537.36",
@@ -15,9 +18,9 @@ class Link:
 
     def get_online_page(self):
         sion = requests.session()
-        sion.post("http://172.30.0.2:8080/selfservice/module/scgroup/web/login_judge.jsf",
+        sion.post(self.link+self.login,
                   headers=self.header, data=self.data)
-        page = sion.get("http://172.30.0.2:8080/selfservice/module/webcontent/web/onlinedevice_list.jsf",
+        page = sion.get(self.link+self.query,
                         headers=self.header)
         return page
 
