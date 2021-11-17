3fde67da6fd530280f4fb3c2c8f0d6c39467572a
Lao_Dong <xianyudelaodong@outlook.com>
2021-11-17 11:17:35 +0800

    添加其他学校版本，理论通杀锐捷认证系统

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from link import Link
 
 
 if __name__ == '__main__':
+    link = input("输入校园网认证页面<自助服务>网址\n"
+                 "类似http://172.30.0.2:8080/selfservice/\n一定要加上http和selfservice\nInput: ")
     username = input("Username: ")
     password = input("Password: ")
-    online = Link(username, password)
+    online = Link(link, username, password)
     online_num = online.any_online()
     if not online_num:
         print("没人在线")
